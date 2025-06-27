7e03c87794819e62200ab2c486f7822ee53fcc9a
Mani Movassagh <mani.mgh@gmail.com>
2025-06-27 23:49:12 +0200

    Refactor WeatherForecast record definition and adjust namespace placement

--- Program.cs
+++ Program.cs
@@ -30,7 +30,10 @@ app.MapGet("/about", () => new { message = "About page" })
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+namespace dotnetTenTest
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
 }
