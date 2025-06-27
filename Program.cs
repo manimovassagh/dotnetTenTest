1cd338ecebfe1b2589277972fe9f247940b224c1
Mani Movassagh <mani.mgh@gmail.com>
2025-06-27 23:48:09 +0200

    Remove unnecessary comment about OpenAPI configuration

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
