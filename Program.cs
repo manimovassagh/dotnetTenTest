c209fff72886e19ede620cb03c1e1fab41d1caec
Mani Movassagh <mani.mgh@gmail.com>
2025-06-27 23:47:53 +0200

    Remove redundant comment about adding services to the container

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
