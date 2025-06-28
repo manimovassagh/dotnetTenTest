240ed53a04220ee92aae3ef09843cec873629acb
Mani Movassagh <mani.mgh@gmail.com>
2025-06-28 18:56:01 +0200

    Update AssemblyInformationalVersion and cache input hash

--- obj/Debug/net10.0/dotnetTenTest.AssemblyInfo.cs
+++ obj/Debug/net10.0/dotnetTenTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetTenTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e03c87794819e62200ab2c486f7822ee53fcc9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4556a54eae79284ea3ea5dab0ed5bc06b15e9e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetTenTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetTenTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
